d97462502557ae23ee65ec3397db8bb5d380d380
Takayuki Minami <takayuki.minami.jz@renesas.com>
2026-09-12 19:14:08 +0900

    Update project

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 /*1
 /*2
 /*3
-* Copyright (c) 2020(2022) Renesas Electronics Corporation and/or its affiliates
+/*4
+pyright (c) 2020(2022) Renesas Electronics Corporation and/or its affiliates
 *
 * SPDX-License-Identifier: BSD-3-Clause
 */
