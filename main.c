797b2c054000a7e920e8bf3bb065e01da3bb81f8
Takayuki Minami <takayuki.minami.jz@renesas.com>
2026-09-12 19:33:50 +0900

    Update project

--- main.c
+++ main.c
@@ -1,29 +1,3 @@
 /*
 /*1
 /*2
-/*3
-/*4
-pyright (c) 2020(2022) Renesas Electronics Corporation and/or its affiliates
-*
-* SPDX-License-Identifier: BSD-3-Clause
-*/
-/***********************************************************************/
-/*                                                                     */
-/*  FILE        :Main.c or Main.cpp                                    */
-/*  DATE        :                                                      */
-/*  DESCRIPTION :Main Program                                          */
-/*  CPU TYPE    :                                                      */
-/*                                                                     */
-/*  NOTE:THIS IS A TYPICAL EXAMPLE.                                    */
-/*                                                                     */
-/***********************************************************************/
-
-void main(void);
-
-void main(void)
-{
-
-}
-/*3*/
-/*4*/
-/*5*/
